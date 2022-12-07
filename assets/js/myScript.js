991dea68c42855ca0642fec57b3240e971f05660
Balkeushna <balkrushnabhovad@gmail.com>
2022-12-08 02:04:51 +0530

    first commit

---
+++ assets/js/myScript.js
@@ -0,0 +1,45 @@
+
+    // OWL Carousel started
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            },
+            1350: {
+                items: 3
+            }
+        }
+    })
+
+
+    // $('.owl-carousel').owlCarousel({
+    //     loop: true,
+    //     margin: 20,
+    //     nav: true,
+    //     responsive: {
+    //         0: {
+    //             items: 1
+    //         },
+    //         600: {
+    //             items: 2
+    //         },
+    //         1000: {
+    //             items: 3
+    //         },
+    //         1350: {
+    //             items: 3
+    //         }
+    //     }
+    // })
+        // OWL Carousel end
+
+
